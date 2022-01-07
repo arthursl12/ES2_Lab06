a772f9e0bac4d7d3495bc7978c05df9bb60c21fa
arthursl12 <arthursoutolima@gmail.com>
2022-01-07 12:50:51 -0300

    Subtraction

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
 class Calculator:
     def sum(self, a, b):
         return a + b
+
+    def subtraction(self, a, b):
+        return a - b
--- test_calculator.py
+++ test_calculator.py
@@ -6,3 +6,9 @@ class TestCalculator:
     
     def test_sum(self):
         assert(self.calc.sum(1,2) == 3)
+    
+    def test_subtraction(self):
+        assert(self.calc.subtraction(10,3) == 7)
+    
+    def test_subtraction_negative_result(self):
+        assert(self.calc.subtraction(2,6) == -4)
